553c803d64e1ce4e6487d64baebfc211f4d3e558
Marcin-Grabowski <mrg.grabowski@gmail.com>
2025-03-07 11:32:26 +0100

    Dodano nową funkcjonalność

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// function myFunction(element) {
-//     var x = document.getElementById("myTopnav");
-
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//         element.innerHTML = "close"; // zmień ikonę na "close"
-//         element.setAttribute("aria-label", "Zamknij menu"); // zaktualizuj tekst dla czytników ekranu
-//     } else {
-//         x.className = "topnav";
-//         element.innerHTML = "menu"; // przywróć ikonę "menu"
-//         element.setAttribute("aria-label", "Otwórz menu"); // przywróć tekst dla czytników ekranu
-//     }
-// }
-
 function myFunction(element) {
     var x = document.getElementById("myTopnav");
 
@@ -26,7 +12,6 @@ function myFunction(element) {
     }
 }
 
-// Dodaj ten nowy kod
 document.addEventListener('click', function (event) {
     var topnav = document.getElementById("myTopnav");
     var header = document.querySelector(".main-header");
